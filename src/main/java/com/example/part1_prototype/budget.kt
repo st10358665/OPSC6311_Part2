ea2197b0952a4c1d3e9e17690e986ebcaebebb38
st10358665 <129930703+st10358665@users.noreply.github.com>
2025-04-29 08:12:36 +0000

    Add files via upload

---
+++ src/main/java/com/example/part1_prototype/budget.kt
@@ -0,0 +1,11 @@
+package com.example.part1_prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class budget : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.budget)
+    }
+}
---
+++ src/main/java/com/example/part1_prototype/contact_us.kt
@@ -0,0 +1,11 @@
+package com.example.part1_prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class contact_us : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.contact_us)
+    }
+}
